ded1ca25fc12020d8ff2ce9beed055b36f40a13c
JokerMas <konbrosmab@gmail.com>
2023-01-28 17:12:58 +0700

    video 3

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="success" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>The Loy</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">Design by BoyloyGroup</div>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
